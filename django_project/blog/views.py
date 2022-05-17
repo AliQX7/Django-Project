c82ef14f50ed3fdcb4830b499144c1bbdbba92f2
unknown <ali.haider.7000@gmail.com>
2022-05-17 15:08:14 +0500

    Views updates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -29,7 +29,7 @@ class UserPostListView(ListView):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
 
-class PostDetailView(DetailView):
+class PostDetailView():
     model = Post
 
 class PostCreateView(LoginRequiredMixin, CreateView):
