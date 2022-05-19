5cf1d8c9ba4419b4b2b281911db3e2ba3bd67884
unknown <ali.haider.7000@gmail.com>
2022-05-19 14:11:47 +0500

    Updates

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -13,9 +13,9 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('login')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'users/register.html', {'form': form})
+    # else:
+    #     form = UserRegistrationForm()
+    # return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
